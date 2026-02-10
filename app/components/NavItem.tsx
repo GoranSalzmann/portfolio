4ff652cbbd3a65f1c8a9ad088c8a47f32484a6af
Mike Karl <karl.mike2002@gmail.com>
2026-02-10 10:58:33 +0100

    feat(Home): add GitHub and LinkedIn links with icons, adjust job title animation position

--- app/components/NavItem.tsx
+++ app/components/NavItem.tsx
@@ -10,7 +10,7 @@ export default function NavItem({
     href: string;
 }>) {
     const highlighted = usePathname() === href;
-    return <Link href={href} className={`text-lg mx-4 flex font-semibold ${highlighted && 'underline underline-offset-8'}`}>
+    return <Link href={href} className={`text-lg mx-4 flex font-semibold ${highlighted ? 'underline underline-offset-8':""} hover:scale-110 active:rotate-4`}>
         {children}
     </Link>
 }
